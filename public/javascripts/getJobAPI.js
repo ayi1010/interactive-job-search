9f7972b82949009a7f6708ad8af6059bd4dd528d
Aerim Yi <aerimyi@192-168-1-104.tpgi.com.au>
2022-03-24 15:14:47 +1100

    added map and navigation control

---
+++ public/javascripts/getJobAPI.js
@@ -0,0 +1,12 @@
+// const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+// const mbxToken = process.env.mapbox_token
+// const geocoder = mbxGeocoding({ accessToken: mbxToken });
+
+// const createMap = async (req, res) => {
+//     const geoData = await geocoder.forwardGeocode({
+//         query: 'Sydney, Australia',
+//         limit: 1
+//     }).send()
+//     console.log(geoData)
+//     res.send("OK!!")
+// }
