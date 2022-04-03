a3d9feef2711640d29e37fa1f1a250091f4a8e8a
Aerim Yi <aerimyi@192-168-1-104.tpgi.com.au>
2022-04-03 20:09:12 +1000

    created MarkerManager class

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,3 +1,4 @@
+// display the map
 mapboxgl.accessToken = mbxToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
@@ -9,49 +10,4 @@ const map = new mapboxgl.Map({
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
-const formSubmit = document.querySelector('#formSubmit')
-let jobs;
-formSubmit.addEventListener('submit', async (event) => {
-    console.log('submitted!!')
-    event.preventDefault();
-    const what = document.querySelector('#search').value
-    jobs = await getJobs(what)
-
-    const geojson =
-    {
-        type: 'FeatureCollection',
-        features: jobs.map(job => {
-            if (job.latitude && job.longitude) {
-                const geojson_feature = {
-                    'type': 'Feature',
-                    'properties': {
-                        'title': `${job.title}`,
-                        'description':
-                            `${job.description}`
-                    },
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [job.longitude, job.latitude]
-                    }
-                }
-                return geojson_feature
-            }
-        }).filter(job => job != undefined)
-    }
-
-    // add markers to map
-    geojson.features.forEach(feature => {
-        // create a HTML element for each feature
-        const el = document.createElement('div');
-        el.className = 'marker';
-
-        // make a marker for each feature and add to the map
-        new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).setPopup(
-            new mapboxgl.Popup({ offset: 25 }) // add popups
-                .setHTML(
-                    `<h5>${feature.properties.title}</h5><p>${feature.properties.description}</p>`
-                )
-        ).addTo(map);
-    })
-})
 
---
+++ public/javascripts/markerManager.js
@@ -0,0 +1,58 @@
+class MarkerManager {
+    constructor(currentId = 0) {
+        this.currentId = currentId;
+        this.markers = [];
+    }
+    createGeojson(jobData) {
+        // refactor with reduce method
+        const geojson =
+        {
+            type: 'FeatureCollection',
+            features: jobData.map(job => {
+                if (job.latitude && job.longitude) {
+                    const geojson_feature = {
+                        'type': 'Feature',
+                        'properties': {
+                            'title': `${job.title}`,
+                            'description':
+                                `${job.description}`,
+                            'url': `${job.redirect_url}`
+                        },
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [job.longitude, job.latitude]
+                        }
+                    }
+                    return geojson_feature
+                }
+            }).filter(job => job != undefined)
+        }
+        return geojson
+    }
+    addMarkers(data) {
+        console.log(data)
+        console.log(data.features)
+        data.features.forEach(feature => {
+            // set marker
+            const marker = new mapboxgl.Marker()
+                .setLngLat(feature.geometry.coordinates)
+
+            // add popups & add to map
+            marker.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+                .setHTML(
+                    `<h5>${feature.properties.title}</h5><p>${feature.properties.description}</p><a href="${feature.properties.url}" target="_blank">Go to Job ad...</a>`
+                )
+            ).addTo(map);
+
+            // add markers
+            this.markers.push(marker)
+        })
+    }
+    deleteMarkers() {
+        // for (let i=0; i < this.markers.length; i++){
+        //     this.markers.remove();
+        // }
+        this.markers.forEach(marker => { marker.remove() })
+    }
+}
+
